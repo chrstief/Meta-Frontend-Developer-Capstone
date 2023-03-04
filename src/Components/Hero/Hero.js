c4d893c6b07b638819db320dce43d0570c521d30
chrstief <chrstief@gmail.com>
2023-03-04 14:15:57 +0000

    generalize container class

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,7 +5,7 @@ import Button from "../Button/Button";
 export default function Hero() {
   return (
     <section style={{width:"100%", backgroundColor:"var(--primary-color-green)"}}>
-      <div className='container' style={{padding: "24px 0", display: "grid", gridTemplateColumns:"1fr 1fr", gap:"24px"}}>
+      <div className='container' style={{display: "grid", gridTemplateColumns:"1fr 1fr", gap:"24px"}}>
         <div><h1 className='displayTitle' style={{color:"var(--primary-color-yellow)"}}>Little Lemon</h1>
           <h2 className='subTitle' style={{color:'white'}}>Chicago</h2>
           <p className='leadText' style={{color:'white', padding:"16px 0"}}>We are a family owned Mediteranian restaurant, focused on traditional recipies served with a modern twist.</p>

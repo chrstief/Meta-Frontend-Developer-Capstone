3f1c4962e3b10b5df08508e9fe9c19efc71aa123
chrstief <chrstief@gmail.com>
2023-03-04 15:32:37 +0000

    fix code formatting

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,12 +6,13 @@ export default function Hero() {
   return (
     <section className={styles.heroBackground}>
       <div className={`container ${styles.grid}`}>
-        <div><h1 className={`displayTitle ${styles.heading}`}>Little Lemon</h1>
+        <div>
+          <h1 className={`displayTitle ${styles.heading}`}>Little Lemon</h1>
           <h2 className={`subTitle ${styles.subHeading}`}>Chicago</h2>
           <p className={`leadText ${styles.text}`}>We are a family owned Mediteranian restaurant, focused on traditional recipies served with a modern twist.</p>
           <div>
-          <Button to="/BookingPage">Reserve a Table</Button>
-        </div>
+            <Button to="/BookingPage">Reserve a Table</Button>
+          </div>
         </div>
         <img src={RestaurantFood} alt="Restaurant Food" className={styles.heroImage}/>
       </div>

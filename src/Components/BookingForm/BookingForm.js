f345472b35109fcf7eb91e44a72c37978baaccc4
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-03-08 18:35:05 +0000

    controlled form

--- src/Components/BookingForm/BookingForm.js
+++ src/Components/BookingForm/BookingForm.js
@@ -1,27 +1,73 @@
+import { useState } from 'react';
 import styles from './BookingForm.module.css';
 
 export default function BookingForm() {
+  const [formData, setFormData] = useState({
+    resDate: '',
+    resTime: '17:00',
+    guests: '1',
+    occasion: 'Birthday'
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData); // Do something with the form data here
+  };
+
   return (
-    <form style={{display: "grid", maxWidth: "200px", gap: "20px"}}>
-        <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date"/>
-        <label htmlFor="res-time">Choose time</label>
-        <select id="res-time ">
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
-        </select>
-        <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests"/>
-        <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
-            <option>Birthday</option>
-            <option>Anniversary</option>
-        </select>
-        <input type="submit" value="Make Your reservation"/>
+    <form
+      style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        name="resDate"
+        value={formData.resDate}
+        onChange={handleChange}
+      />
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time"
+        name="resTime"
+        value={formData.resTime}
+        onChange={handleChange}
+      >
+        <option value="17:00">17:00</option>
+        <option value="18:00">18:00</option>
+        <option value="19:00">19:00</option>
+        <option value="20:00">20:00</option>
+        <option value="21:00">21:00</option>
+        <option value="22:00">22:00</option>
+      </select>
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        name="guests"
+        value={formData.guests}
+        onChange={handleChange}
+      />
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        name="occasion"
+        value={formData.occasion}
+        onChange={handleChange}
+      >
+        <option value="Birthday">Birthday</option>
+        <option value="Anniversary">Anniversary</option>
+      </select>
+      <input type="submit" value="Make Your reservation" />
     </form>
-  )
+  );
 }

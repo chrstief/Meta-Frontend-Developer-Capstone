398a000e3e59843b4b59d838df4c8248cdcaa617
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-03-12 18:36:26 +0000

    styling

--- src/Pages/ConfirmedBooking/ConfirmedBooking.js
+++ src/Pages/ConfirmedBooking/ConfirmedBooking.js
@@ -2,6 +2,7 @@ import styles from '../Page.module.css';
 import Footer from '../../Components/Footer/Footer';
 import Header from '../../Components/Header/Header';
 import Nav from '../../Components/Nav/Nav';
+import styles2 from './ConfirmedBooking.module.css';
 
 export default function ConfirmedBooking() {
 
@@ -11,7 +12,7 @@ export default function ConfirmedBooking() {
         <Header/>
         <Nav/>
       </div>
-      <h1>
+      <h1 className={`displayTitle ${styles2.heroBackground}`}>
         Booking confirmed!
       </h1>
       <Footer/>
